4fb974397841808a021bd82e6093fc06c9cc8344
YESWANTHATTADA <119840544+YESWANTHATTADA@users.noreply.github.com>
2024-01-06 12:02:23 +0530

    import java.util.*;  public class Example2{ 	public static void main(String[] args){
    		Scanner sc = new Scanner(System.in);
    		int a = sc.nextInt(); // to read an integer
    		int b = sc.nextInt();
    		System.out.println(a%b);
    		
    	}
    }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class Example2{
+	public static void main(String[] args){
+	
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(); // to read an integer
+		int b = sc.nextInt();
+		System.out.println(a%b);
+		
+	}
+}
